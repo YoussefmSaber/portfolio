385dd086fb8f179bffe09e195be6fec0369abc1f
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-02-13 15:27:16 +0200

    Initial commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,46 @@
+import {Canvas} from "@react-three/fiber";
+import {PerspectiveCamera} from "@react-three/drei";
+import HackerRoom from "../component/HackerRoom.jsx";
+import {Suspense} from "react";
+import CanvasLoader from "../component/CanvasLoader.jsx";
+import {useMediaQuery} from "react-responsive";
+import {calculateSizes} from "../constants/index.js";
+import Button from "../component/Button.jsx";
+
+const Hero = () => {
+    const isSmall = useMediaQuery({maxWidth: 440})
+    const isMobile = useMediaQuery({minWidth: 440, maxWidth: 768})
+    const isTablet = useMediaQuery({minWidth: 768,maxWidth: 1024})
+
+    const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+    return (
+        <section className={"min-h-screen w-full flex flex-col relative"}>
+            <div className={"w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3"}>
+                <p className={"sm:text-3xl text-xl font-medium text-white text-center font-generalsans"}>Hi, I am Youssef <span className={"waving-hand"}>👋</span></p>
+                <p className={"hero_tag text-gray_gradient"}>An Mobile Android Developer</p>
+            </div>
+            <div className={"w-full h-full absolute inset-0"}>
+
+                <Canvas className={"w-full h-full"}>
+                    <Suspense fallback={<CanvasLoader />}>
+                        <PerspectiveCamera makeDefault position={[0, 0, 25]}/>
+                        <HackerRoom
+                            scale={sizes.deskScale}
+                            position={sizes.deskPosition}
+                            rotation={[Math.PI/12, -Math.PI, 0]}
+                        />
+                        <ambientLight intensity={1} />
+                        <directionalLight position={[10, 10, 10]} intensity={0.5} />
+                    </Suspense>
+                </Canvas>
+            </div>
+            <div className={"absolute bottom-7 left-0 right-0 w-full z-10 c-space"}>
+                <a href={"#contact"} className={"w-fit"}>
+                    <Button name={"Let's work together"} isBeam containerClass={"sm:w-fit w-full sm:min-w-96"}/>
+                </a>
+            </div>
+        </section>
+    )
+}
+export default Hero
